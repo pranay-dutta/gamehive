54fd41fa39496865f43eb2c027eaceb964e0e523
Pranay Dutta <rick15176@gmail.com>
2025-02-20 02:55:36 +0530

    feat: add navbar dynamic shadow

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "@/assets/logo.png";
-import { ColorModeButton } from "@/components/ui/color-mode";
+import { ColorModeButton, useColorModeValue } from "@/components/ui/color-mode";
 
 const Navbar = () => {
+  const shadow = useColorModeValue("0px 2px 4px rgba(0, 0, 0, 0.05)", "0px 2px 4px rgba(255, 255, 255, 0.05)");
+
   return (
-    <HStack px={4} justifyContent={"space-between"} boxShadow="xs">
+    <HStack px={4} py={2} justifyContent={"space-between"} boxShadow={shadow}>
       <HStack gap={15}>
         <Image src={logo} boxSize=" 50px" borderRadius="50%"></Image>
-        <Text>Navbar</Text>
+        <Text fontWeight="medium">GameHive</Text>
       </HStack>
       <ColorModeButton borderRadius="50%" />
     </HStack>
